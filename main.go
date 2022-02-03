1682964d731dadc15ec85232ffd6aa7fa1ed2229
Mike Severof <mee8a1@gmail.com>
2022-02-03 16:34:32 +0300

    change readme

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// https://go.dev/doc/tutorial/web-service-gin
 package main
 
 import (
@@ -24,6 +25,7 @@ var albums = []album{
 func main() {
     router := gin.Default()
     router.GET("/albums", getAlbums)
+    router.POST("/albums", postAlbums)
 
     router.Run("localhost:8080")
 }
@@ -32,3 +34,23 @@ func main() {
 func getAlbums(c *gin.Context) {
     c.IndentedJSON(http.StatusOK, albums)
 }
+
+// postAlbums adds an album from JSON received in the request body.
+func postAlbums(c *gin.Context) {
+    var newAlbum album
+
+    // Call BindJSON to bind the received JSON to
+    // newAlbum.
+    if err := c.BindJSON(&newAlbum); err != nil {
+        return
+    }
+
+    // Add the new album to the slice.
+    albums = append(albums, newAlbum)
+    c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+/*
+try it with
+$ curl http://localhost:8080/albums --include --header "Content-Type: application/json" --request "POST" --data '{"id": "4","title": "The Modern Sound of Betty Carter","artist": "Betty Carter","price": 49.99}'
+*/
